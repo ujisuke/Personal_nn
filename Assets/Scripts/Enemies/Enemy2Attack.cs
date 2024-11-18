ad816b16fbe48d00dd732c526da6ad88f6348343
ujisuke <86547644+ujisuke@users.noreply.github.com>
2024-11-19 02:13:44 +0900

    Enemy2の精度を向上した

--- Assets/Scripts/Enemies/Enemy2Attack.cs
+++ Assets/Scripts/Enemies/Enemy2Attack.cs
@@ -28,27 +28,31 @@ namespace Assets.Scripts.Enemies
         }
 
         private IEnumerator Attack()
-        {
-            Vector3 fireRePos3 = transform.position + new Vector3(0f, 0f, 1f);
-            Vector3 imDirection3 = ObjectMove.CalclateImDirection3BetWeenTwoRePos3(fireRePos3, ObjectFacade.GetPlayerRePos3()).normalized / 10f;
-            Vector3 enemy2ImPos3 = ObjectMove.ConvertToImPos3FromRePos3(fireRePos3);
-            Vector3 targetImPos3 = ObjectMove.ConvertToImPos3FromRePos3(fireRePos3);
-            Vector3 playerImPos3 = ObjectMove.ConvertToImPos3FromRePos3(ObjectFacade.GetPlayerRePos3());
-            while(true)
+        {   
+            for(int i = 0; i < 3; i++)
             {
-                targetImPos3 += imDirection3;
-                if((targetImPos3 - playerImPos3).magnitude <= 0.1f || ObjectMove.IsHitWall(targetImPos3))
-                    break;
+                yield return new WaitForSeconds(0.3f);
+                Vector3 fireRePos3 = transform.position + new Vector3(0f, 0f, 1f);
+                Vector3 imDirection3 = ObjectMove.CalclateImDirection3BetWeenTwoRePos3(fireRePos3, ObjectFacade.GetPlayerRePos3()).normalized * 0.2f;
+                Vector3 enemy2ImPos3 = ObjectMove.ConvertToImPos3FromRePos3(fireRePos3);
+                Vector3 targetImPos3 = ObjectMove.ConvertToImPos3FromRePos3(fireRePos3);
+                Vector3 playerImPos3 = ObjectMove.ConvertToImPos3FromRePos3(ObjectFacade.GetPlayerRePos3());
+                while(true)
+                {
+                    targetImPos3 += imDirection3;
+                    if((targetImPos3 - playerImPos3).magnitude <= 0.1f || ObjectMove.IsHitWall(targetImPos3))
+                        break;
+                }
+                yield return new WaitForSeconds(0.1f);
+                targetImPos3 = enemy2ImPos3;
+                while(true)
+                {
+                    targetImPos3 += imDirection3;
+                    if((targetImPos3 - playerImPos3).magnitude <= 0.1f || ObjectMove.IsHitWall(targetImPos3))
+                        break;
+                }           
+                Instantiate(damageObjectPrefab, ObjectMove.ConvertToRePos3FromImPos3(targetImPos3), Quaternion.identity);
             }
-            yield return new WaitForSeconds(0.5f);
-            targetImPos3 = enemy2ImPos3;
-            while(true)
-            {
-                targetImPos3 += imDirection3;
-                if((targetImPos3 - playerImPos3).magnitude <= 0.1f || ObjectMove.IsHitWall(targetImPos3))
-                    break;
-            }            
-            Instantiate(damageObjectPrefab, ObjectMove.ConvertToRePos3FromImPos3(targetImPos3), Quaternion.identity);
             isAttacking = false;
         }
     }
